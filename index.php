dd20157c7d55ba7f5bc2b912973e4a1505b078d6
haoran317517405 <haoran.tang@uqconnect.edu.au>
2018-03-12 17:48:35 +1000

    Added initial files

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <head>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-</head>
+</head>INFS7901 Course List</title>
 <body>
 
     <h1>INFS7901 - Course List</h1>
